c0e75d9ad8089cf44c51b3dc0ddfedb4366568f7
Sophia Fang <sophiaqfang@gmail.com>
2018-06-30 07:47:50 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,9 +1,5 @@
 def using_until
-        levitation_force = 6
-        #your code here
-  while levitation_force < 10
-    levitation_force += 1
-  end
-  puts 
+  levitation_force = 6
+  #your code here
 end
 
